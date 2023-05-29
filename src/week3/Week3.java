f9a8689491dbfdacbdc22e159123bb3cf1e3a60b
sin13635 <sin13635@sheridancollege.ca>
2023-05-29 12:47:30 -0400

    my code heh

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -15,6 +15,7 @@ public class Week3 {
      */
     public static void main(String[] args) {
         System.out.println("learning github operations");
+        System.out.println("learning operations");
     }
     
 }
